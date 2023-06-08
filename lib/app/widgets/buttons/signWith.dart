0d87811c86f6abe8e94d6f44d6edcae18a7fbbfd
BestCod3 <erbol.kubatovv@mail.ru>
2023-06-08 23:46:01 +0600

    add configurations

--- lib/app/widgets/buttons/signWith.dart
+++ lib/app/widgets/buttons/signWith.dart
@@ -13,7 +13,7 @@ class SignWith extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.08,
             width: MediaQuery.of(context).size.width * 0.4,
             decoration: BoxDecoration(
-              boxShadow: [
+              boxShadow: const [
                 BoxShadow(
                     color: Colors.black54,
                     offset: Offset(0, 1),
@@ -25,36 +25,9 @@ class SignWith extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(15),
               child: image,
-              // child: Image.network(
-              //   'https://cdn-icons-png.flaticon.com/128/2991/2991148.png',
-              // ),
             ),
           ),
         ),
-        // SizedBox(
-        //   width: 30,
-        // ),
-        // InkWell(
-        //   child: Container(
-        //     height: MediaQuery.of(context).size.height * 0.08,
-        //     width: MediaQuery.of(context).size.width * 0.4,
-        //     decoration: BoxDecoration(
-        //       boxShadow: [
-        //         BoxShadow(
-        //             color: Colors.black54,
-        //             offset: Offset(0, 1),
-        //             blurRadius: 2.0),
-        //       ],
-        //       color: Colors.white,
-        //       borderRadius: BorderRadius.circular(7),
-        //     ),
-        //     child: Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: Image.network(
-        //           "https://cdn-icons-png.flaticon.com/128/4494/4494475.png"),
-        //     ),
-        //   ),
-        // ),
       ],
     );
   }
